a006c84c5d9de9aa258ed840991b6d34707e5205
ljt528 <ljt528@naver.com>
2023-10-29 19:54:05 +0900

    Docs : 문서 수정

--- mywebsite/mywebsite/settings.py
+++ mywebsite/mywebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from pathlib import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "blog.apps.BlogConfig",
+    "widget_tweaks",
 ]
 
 MIDDLEWARE = [
@@ -104,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
@@ -122,3 +124,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
--- mywebsite/mywebsite/urls.py
+++ mywebsite/mywebsite/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from django.views.generic RedirectView
+from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
 
 # https://localhost:8000/
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include("blog.urls"),
-    path('admin/', RedirectView.as_view(url="/blog/", permanent = True)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('blog/', include("blog.urls")),
+    path('', RedirectView.as_view(url="/blog/", permanent = True)),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
